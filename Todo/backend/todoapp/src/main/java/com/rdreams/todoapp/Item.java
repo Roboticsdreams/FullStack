42dd5184fc45e295ebc6289026e42c13f0d29f34
roboticsdreams <p.rathinavel@gmail.com>
2024-12-07 11:15:25 +0530

    todoapp

---
+++ Todo/backend/todoapp/src/main/java/com/rdreams/todoapp/Item.java
@@ -0,0 +1,24 @@
+package com.rdreams.todoapp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private boolean done;
+}
